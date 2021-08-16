73d4406acd2ac97ff94d9e9245629954557b0ae7
Henrique <henrique.c.ladeira@gmail.com>
2021-08-16 20:44:38 -0300

    ✨ feat: configure store to use redux saga

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,20 @@
-import { createStore } from '@reduxjs/toolkit';
+import { applyMiddleware, compose, createStore } from '@reduxjs/toolkit';
 import rootReducer from './root.reducer';
+import rootSaga from './root.saga';
 import reactotron from '../config/reactotron';
+import createSagaMiddleware from '@redux-saga/core';
 
-const middleware = reactotron.createEnhancer!();
+const sagaMonitor = reactotron.createSagaMonitor!();
 
-const store = createStore(rootReducer, middleware);
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+
+const middlewares = compose(
+  applyMiddleware(sagaMiddleware),
+  reactotron?.createEnhancer!(),
+);
+
+const store = createStore(rootReducer, middlewares);
+sagaMiddleware.run(rootSaga);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
