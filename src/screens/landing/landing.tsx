8622a5660c8cdf7fe9c6bc09f422a532e2d7c473
Henrique <henrique.c.ladeira@gmail.com>
2021-08-22 17:34:44 -0300

    🩹 fix: remove unused import
    still have to remove that inline style 🤯

--- src/screens/landing/landing.tsx
+++ src/screens/landing/landing.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import { Button, useTheme } from 'react-native-paper';
 
 interface Props {
