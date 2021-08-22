7e2948543ebffea2319cc58db6de7b3da40818b8
Henrique <henrique.c.ladeira@gmail.com>
2021-08-22 19:35:18 -0300

    ✨ feat: add HomeTabs

---
+++ src/routes/home-tabs.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+import { LandingPage } from '../screens/landing';
+import { Settings } from '../screens/settings';
+
+type RootTabParamList = {
+  Landing: undefined;
+  Settings: undefined;
+};
+
+const BottomTab = createMaterialBottomTabNavigator<RootTabParamList>();
+
+export const HomeTabs = () => (
+  <BottomTab.Navigator>
+    <BottomTab.Screen
+      name="Landing"
+      component={LandingPage}
+      options={{
+        tabBarIcon: ({ color }) => (
+          <MaterialIcons name="blur-circular" color={color} size={26} />
+        ),
+      }}
+    />
+    <BottomTab.Screen
+      name="Settings"
+      component={Settings}
+      options={{
+        tabBarIcon: ({ color }) => (
+          <MaterialIcons name="settings" color={color} size={26} />
+        ),
+      }}
+    />
+  </BottomTab.Navigator>
+);
