f7efd185d379b47e436327ff1961a8b9d6ee64ce
Henrique <henrique.c.ladeira@gmail.com>
2021-10-10 21:36:25 -0300

    ✨ feat: add button to crash the app

--- src/screens/landing/landing.tsx
+++ src/screens/landing/landing.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, { useState } from 'react';
 import { SafeAreaView, View } from 'react-native';
 import { Button, useTheme } from 'react-native-paper';
@@ -10,6 +11,7 @@ interface Props {
 export const LandingPage = ({ navigation }: Props) => {
   const [enabled, setEnabled] = useState(true);
   const { colors } = useTheme();
+  const [errorThrown, setErrorThrown]: any = useState({ message: 'Crash' });
   return (
     <SafeAreaView style={{ backgroundColor: colors.background, flex: 1 }}>
       <Button
@@ -26,6 +28,14 @@ export const LandingPage = ({ navigation }: Props) => {
         }}>
         <LightBulb enabled={enabled} />
         <Switch enabled={enabled} onPress={() => setEnabled(!enabled)} />
+        <Button
+          icon="death-star-variant"
+          mode="contained"
+          onPress={() => {
+            setErrorThrown(undefined);
+          }}>
+          {errorThrown.message}
+        </Button>
       </View>
     </SafeAreaView>
   );
